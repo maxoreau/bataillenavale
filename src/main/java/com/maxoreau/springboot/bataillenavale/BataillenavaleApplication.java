1b57c9c81a9bdbcfe8a12a1047d7a983255a808f
maxoreau <maxime.horeau@gmail.com>
2017-06-06 11:51:25 +0200

    first commit

---
+++ src/main/java/com/maxoreau/springboot/bataillenavale/BataillenavaleApplication.java
@@ -0,0 +1,12 @@
+package com.maxoreau.springboot.bataillenavale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BataillenavaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BataillenavaleApplication.class, args);
+	}
+}
