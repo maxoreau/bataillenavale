841a7ac074ae61b5860a376497f82e9688fd9226
maxoreau <maxime.horeau@gmail.com>
2017-06-27 08:47:31 +0200

    clean up delete workspace for ib to be up to date with home workspace

--- src/main/java/com/maxoreau/springboot/bataillenavale/repositories/GridRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maxoreau.springboot.bataillenavale.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.maxoreau.springboot.bataillenavale.models.Grid;
-
-public interface GridRepository extends CrudRepository<Grid, Long> {
-
-}
