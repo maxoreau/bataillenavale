ebcf3395e64c1e82a14aab6d305b06f218443680
maxoreau <maxime.horeau@gmail.com>
2017-06-27 10:12:24 +0200

    added code in commandlinerunner for testing

--- src/main/java/com/maxoreau/springboot/bataillenavale/BataillenavaleApplication.java
+++ src/main/java/com/maxoreau/springboot/bataillenavale/BataillenavaleApplication.java
@@ -29,73 +29,132 @@ public class BataillenavaleApplication {
 	@Bean
 	public CommandLineRunner demo(PlayerRepository playerRepo, GameRepository gameRepo, GameManager gameManager) {
 		return (args) -> {
+
+			 Player p1 = new Player();
+			 p1.setName("bob");
 			
-			System.out.println(gameManager.getAllGames().get(0).getGridPlayer1().getBoats().get(0).getOrientation());
-			System.out.println(gameManager.getAllGames().get(0).getGridPlayer1().getLocations().get(0).getNature());
+			 Player p2 = new Player();
+			 p2.setName("chuck");
+			
+			 Player p3 = new Player();
+			 p3.setName("ruth");
+			
+			 Player p4 = new Player();
+			 p4.setName("koost");
+			
+			 playerRepo.save(p1);
+			 playerRepo.save(p2);
+			 playerRepo.save(p3);
+			 playerRepo.save(p4);
+			
+			 Long debut = System.currentTimeMillis();
+			 Game g1 = p1.createGame();
+			 Long fin = System.currentTimeMillis();
+			 gameRepo.save(g1);
+			 System.out.println("jeu généré en " + (fin - debut) + " millisecondes");
+			 p2.enterGame(g1);
+			 gameRepo.save(g1);
+			
+			
+			 do {
+			 int colA = ThreadLocalRandom.current().nextInt(0,
+			 (Parameters.getGridSize()));
+			 int rowA = ThreadLocalRandom.current().nextInt(0,
+			 (Parameters.getGridSize()));
+			 p1.fire(colA, rowA);
+			 System.out.println("player2's grid");
+			 g1.getGridPlayer2().displayOwnGrid();
+			
+			 int colB = ThreadLocalRandom.current().nextInt(0,
+			 (Parameters.getGridSize()));
+			 int rowB = ThreadLocalRandom.current().nextInt(0,
+			 (Parameters.getGridSize()));
+			 p2.fire(colB, rowB);
+			 System.out.println("player1's grid");
+			 g1.getGridPlayer1().displayOwnGrid();
+			
+			 } while (g1.getStatus().compareTo(GameStatus.ONGOING) == 0);
+			
+			 System.out.println("a");
+			 gameRepo.save(g1);
+			 System.out.println("b");
+			
+			 playerRepo.save(p1);
+			 System.out.println("c");
+			 playerRepo.save(p2);
+			 System.out.println("d");
+			
+			 Game g2 = p1.createGame();
+			
+			 gameRepo.save(g2);
+			
+			 playerRepo.save(p1);
+			
+			
+//			System.out.println(gameManager.getAllGames().get(0).getGridPlayer1().getBoats().get(0).getOrientation());
+//			System.out.println(gameManager.getAllGames().get(0).getGridPlayer1().getLocations().get(0).getNature());
 
 		};
 	}
 }
-			
-			
-//			 Player p1 = new Player();
-//			 p1.setName("bob");
-//			
-//			 Player p2 = new Player();
-//			 p2.setName("chuck");
-//			 
-//			 Player p3 = new Player();
-//			 p3.setName("ruth");
-//			
-//			 Player p4 = new Player();
-//			 p4.setName("koost");
-//
-//			 playerRepo.save(p1);
-//			 playerRepo.save(p2);
-//			 playerRepo.save(p3);
-//			 playerRepo.save(p4);
-//			 
-//			 Long debut = System.currentTimeMillis();
-//			 Game g1 = p1.createGame();
-//			 Long fin = System.currentTimeMillis();
-//			 gameRepo.save(g1);
-//			 System.out.println("jeu généré en " + (fin - debut) + " millisecondes");
-//			 p2.enterGame(g1);
-//			 gameRepo.save(g1);	
-//
-//			
-//			 do {
-//			 int colA = ThreadLocalRandom.current().nextInt(0,
-//			 (Parameters.getGridSize()));
-//			 int rowA = ThreadLocalRandom.current().nextInt(0,
-//			 (Parameters.getGridSize()));
-//			 p1.fire(colA, rowA);
-//			 System.out.println("player2's grid");
-//			 g1.getGridPlayer2().displayOwnGrid();
-//			
-//			 int colB = ThreadLocalRandom.current().nextInt(0,
-//			 (Parameters.getGridSize()));
-//			 int rowB = ThreadLocalRandom.current().nextInt(0,
-//			 (Parameters.getGridSize()));
-//			 p2.fire(colB, rowB);
-//			 System.out.println("player1's grid");
-//			 g1.getGridPlayer1().displayOwnGrid();
-//
-//			 } while (g1.getStatus().compareTo(GameStatus.ONGOING) == 0);
-//			 
-//			 System.out.println("a");
-//			 gameRepo.save(g1);
-//			 System.out.println("b");
-//			 
-//			 playerRepo.save(p1);
-//			 System.out.println("c");
-//			 playerRepo.save(p2);
-//			 System.out.println("d");
-//			 
-//			 Game g2 = p1.createGame();
-//			 
-//			 gameRepo.save(g2);
-//			 
-//			 playerRepo.save(p1);
-//			 
 
+// Player p1 = new Player();
+// p1.setName("bob");
+//
+// Player p2 = new Player();
+// p2.setName("chuck");
+//
+// Player p3 = new Player();
+// p3.setName("ruth");
+//
+// Player p4 = new Player();
+// p4.setName("koost");
+//
+// playerRepo.save(p1);
+// playerRepo.save(p2);
+// playerRepo.save(p3);
+// playerRepo.save(p4);
+//
+// Long debut = System.currentTimeMillis();
+// Game g1 = p1.createGame();
+// Long fin = System.currentTimeMillis();
+// gameRepo.save(g1);
+// System.out.println("jeu généré en " + (fin - debut) + " millisecondes");
+// p2.enterGame(g1);
+// gameRepo.save(g1);
+//
+//
+// do {
+// int colA = ThreadLocalRandom.current().nextInt(0,
+// (Parameters.getGridSize()));
+// int rowA = ThreadLocalRandom.current().nextInt(0,
+// (Parameters.getGridSize()));
+// p1.fire(colA, rowA);
+// System.out.println("player2's grid");
+// g1.getGridPlayer2().displayOwnGrid();
+//
+// int colB = ThreadLocalRandom.current().nextInt(0,
+// (Parameters.getGridSize()));
+// int rowB = ThreadLocalRandom.current().nextInt(0,
+// (Parameters.getGridSize()));
+// p2.fire(colB, rowB);
+// System.out.println("player1's grid");
+// g1.getGridPlayer1().displayOwnGrid();
+//
+// } while (g1.getStatus().compareTo(GameStatus.ONGOING) == 0);
+//
+// System.out.println("a");
+// gameRepo.save(g1);
+// System.out.println("b");
+//
+// playerRepo.save(p1);
+// System.out.println("c");
+// playerRepo.save(p2);
+// System.out.println("d");
+//
+// Game g2 = p1.createGame();
+//
+// gameRepo.save(g2);
+//
+// playerRepo.save(p1);
+//
